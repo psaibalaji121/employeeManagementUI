8accb220cdc23d563c8f25a1f12dfef38f698431
Sai Balaji Peruru <psaibalaji@opsecsecurity.com>
2025-09-06 15:36:10 +0530

    created a project for employee management UI

---
+++ src/main/java/com/example/employeemanagementui/EmployeeManagementUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeemanagementui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/employeemanagementui/EmployeeManagementUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeemanagementui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
